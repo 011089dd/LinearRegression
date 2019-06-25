1d43dd212e8da9095a11ef351984ef74e9a9f93b
DD <52127313+011089dd@users.noreply.github.com>
2019-06-26 02:29:10 +0530

    Added linear regression code.

--- LR.py
+++ LR.py
@@ -3,3 +3,35 @@
 # Importing the libraries
 import matplotlib.pyplot as plt
 import pandas as pd
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,test_size=0.30, random_state=2)
+
+model = LinearRegression()
+model.fit(X_train,Y_train)
+Y_prediction = model.predict(X_test)
+
+Y_pred = model.predict([[21]])
+print(Y_pred)
+
+# Visualising the Training set results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, model.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, model.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
